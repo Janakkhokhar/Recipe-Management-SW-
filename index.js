582b29d1153ea9b09a688b7c8176a9ddf248cebd
Janakkhokhar <151613871+Janakkhokhar@users.noreply.github.com>
2024-09-24 01:13:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const port = 9008;
+
+const passport = require('passport')
+
+const session = require('express-session');
+
+const db = require('./cofig/mongoose');
+const jwt = require('./cofig/passport-jwt-stratergy');
+
+const app = express();
+
+app.use(
+    session({
+        name: "jwtPractical",
+        secret: "jwtPractical",
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 100,
+        },
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+app.use(express.urlencoded())
+
+app.use('/user', require('./routes/user'));
+app.use('/post', require('./routes/post'));
+
+
+app.listen(port,(err)=>{
+    (err) ? console.log("server is connect "): console.log("server is connect");
+      
+})
